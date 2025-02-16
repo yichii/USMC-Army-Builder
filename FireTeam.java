8e2f822225657bbd4204f55f189009759e8068ce
yichiii <55825777+yichii@users.noreply.github.com>
2025-02-16 15:18:40 -0800

    Fireteam class now extends unit abstract class

--- FireTeam.java
+++ FireTeam.java
@@ -13,6 +13,10 @@ public class FireTeam extends Unit{
      */
     FireTeam(String name, List<Marine> marines) {
         boolean containsOfficer = false;
+        if (marines.size() != 4) {
+            throw new IllegalArgumentException("A fire team must consist of four marines.");
+        } 
+
         for (Marine marine: marines) {
             if (marine.getRank().ordinal() >= 1) { // Checks if there is at least one marine is ranked Corporal
                 containsOfficer = true;
@@ -20,15 +24,12 @@ public class FireTeam extends Unit{
             }
         }
         
-        if (marines.size() != 4) {
-            throw new IllegalArgumentException("A fire team must consist of four marines.");
-        } 
         if (containsOfficer == false) {
             throw new IllegalArgumentException("A fire team must consist of at least one ranked Corporal or above.");
         } 
+
         super(name);
-        this.marines = marines;
-                        
+        this.marines = marines;                   
     }
     
     /**
--- Main.java
+++ Main.java
@@ -4,40 +4,18 @@ import java.util.*;
 */
 public class Main {
     public static void main(String[] args) {
-        // Creating Army Generator
-        ArmyGenerator armyGenerator = new ArmyGenerator();
+        ArmyGenerator ag = new ArmyGenerator();
+        FireTeam ft1 = ag.createFireTeam("Firelight", null);
+        System.out.println(ft1);
 
-        // Creating each marine
-        Marine marine5 = armyGenerator.createMarine("Rambo", Rank.LIEUTENANT, 45);
-        Marine marine6 = armyGenerator.createMarine("Ray", "Machine Gun", Rank.SERGEANT, 45);
-        Marine marine7 = armyGenerator.createMarine("Arthur", "Gun", Rank.SERGEANT, 45);
-        
-        // Creating a squad
-        Squad squad = new Squad("Delta Squad", marine5, new ArrayList<FireTeam>(List.of(
-            armyGenerator.createFireTeam("Limma", null), 
-            armyGenerator.createFireTeam("Charlie", null), 
-            armyGenerator.createFireTeam("Omega", null)
-        )));
-        
-        // Adding the fire team to the squad
+        List<Marine> marines = new ArrayList<Marine>(List.of(
+            new Marine("John", "Snow", 22),
+            new Marine("John", "Snow", Rank.SERGEANT, 22),
+            new Marine("John", "Snow", 22),
+            new Marine("John", "Snow", 22)
+        ));
 
-        System.out.println(squad);
-        
-        // Creating a platoon
-        Platoon platoon = new Platoon("1st Platoon", new Headquarters(null, marine5, marine6, marine7));
-        platoon.addSquad(squad);
-        platoon.addSquad(squad);
-        platoon.addSquad(squad);
-        System.out.println(platoon);
-        
-        /* Dilemma: How do we create a platoon that has a squad to be part of it(composition) while also having the fire teams to be part of each
-        * individual squad(composition). How do we do this?
-        * Possible solution #1: have it so you create a squad with the fireteams already intact. We do this by opting for adding a fire team through the
-        * constructor rather through the addFireTeam method. 
-        * 
-        * Drawback: adding a squad will be really cluttered.
-        * 
-        */
-        
+        FireTeam ft2 = new FireTeam(marines);
+        System.out.println(ft2);
     }
 }

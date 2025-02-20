a7dfa6918b91603304ea845ac3f6b8f33c855383
yichiii <55825777+yichii@users.noreply.github.com>
2025-02-19 23:42:20 -0800

    Decided to Start Using Maven - The scope of armybuilder has definitely gotten out of control so using maven will make it easier to manage the project as a whole(I also need hands on practice with using maven)

---
+++ armybuilder/src/test/java/com/armybuilder/AppTest.java
@@ -0,0 +1,20 @@
+package com.armybuilder;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

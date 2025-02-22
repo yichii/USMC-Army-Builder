a4ce75f98b3a2d44b2079c8f643aa8fa6d709c48
yichiii <55825777+yichii@users.noreply.github.com>
2025-02-21 21:49:42 -0800

    Started on the Company Class - still needs to be finished.

--- armybuilder/src/main/java/com/armybuilder/models/Company.java
+++ armybuilder/src/main/java/com/armybuilder/models/Company.java
@@ -1,8 +1,18 @@
 package com.armybuilder.models;
 
+import java.util.*;
 /**
  * A company consists of three rifle platoons plus a weapons platoon. It also consists of a headquarters with 8-10 high ranking marines. 
  */
-public class Company {
+public class Company extends Unit{
+    private CompanyHeadquarters companyHQ;
+    private List<RiflePlatoon> riflePlatoons;
+    private WeaponPlatoon weaponPlatoon;
 
+    Company(String name, CompanyHeadquarters companyHQ, List<RiflePlatoon> riflePlatoons, WeaponPlatoon weaponPlatoon) {
+        super(name);
+        this.companyHQ = companyHQ;
+        this.riflePlatoons = new ArrayList<RiflePlatoon>();
+        this.weaponPlatoon = weaponPlatoon;
+    }
 }
